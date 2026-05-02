63161d59a950b5af8946ddce2c39808d8eac03a8
Shah Sawood <shahsawoodshinwari@gmail.com>
2026-05-03 01:29:25 +0500

    new changes

---
+++ assets/Can-C0ImSHey.js
@@ -0,0 +1,1 @@
+import{z as e,j as s}from"./index-BhPJoSes.js";function t(n){const{has:r}=e();return r(n.permission)?s.jsx(s.Fragment,{children:n.children}):null}export{t as C};
---
+++ assets/index-DnFPO8IK.js
@@ -0,0 +1,1 @@
+import{I as r}from"./index-BhPJoSes.js";var a=r();export{a as r};
